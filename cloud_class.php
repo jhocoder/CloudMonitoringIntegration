5d8cfc6f2745c7ee143cd097e1b276f6772468b5
jhocoder <jhonathanrugeb@gmail.com>
2025-04-15 12:57:41 +0200

    testing class cloud

--- cloud_class.php
+++ cloud_class.php
@@ -1,5 +1,5 @@
 <?php
-// Permitir peticiones desde cualquier origen (útil para desarrollo)
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
